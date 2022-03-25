e9f8144d92de1d26f348a6fc0864f04e4edbdd46
Mark Hermsdorfer <m.hermsdorfer@f5.com>
2022-03-25 12:46:05 -0400

    added ability to write out settings files

--- f5-vip/f5-vip-discovery.py
+++ f5-vip/f5-vip-discovery.py
@@ -2,19 +2,18 @@
 # description: F5 Failover OCI Discovery Script
 # author: m.hermsdorfer@f5.com
 # date: 18-Jan-2022
-# version: 1.0
-# Updated: 18-Jan-2022
+# version: 1.2
+# Updated: 23-Mar-2022
 # Updates:
 # 1.0: Initial release.
-# 1.1: m.hermsdorfer@f5.com - 23-Mar-2022 added example settings.json
+# 1.1: m.hermsdorfer@f5.com - 23-Mar-2022: added example settings.json
+# 1.2: m.hermsdorfer@f5.com - 25-Mar-2022: added ability to write out settings.json with -w argument.
 # =====================================
-from pickle import FALSE, TRUE
 import requests
 import json
 import os
-import signal
 import sys
-import time
+import getopt
 import oci
 
 def get_current_metadata(type):
@@ -28,7 +27,11 @@ def get_current_metadata(type):
 
     call_attempts = 0
     while call_attempts < 2:
-        response = requests.get(uri, headers=signer.METADATA_AUTH_HEADERS)
+        try:
+            response = requests.get(uri, headers=signer.METADATA_AUTH_HEADERS)
+        except Exception as e:
+            print('Script failed to get instance metadata, check routing to ensure request leaves primary instance VNIC & Security Groups are in-place: ' + str(e))
+            sys.exit(1)
         if response.ok:
             json_object = json.loads(response.content)
             json_text = json.dumps(json_object)
@@ -54,39 +57,50 @@ def get_bigip_interfaces():
         else:
             response.raise_for_status()
 
-def main():
-    my_example_settings_json = {}
-    my_example_settings_json['topic_id']="null"
-    my_example_settings_json['multiprocess']="off"
-    my_example_settings_json['timeout_seconds']="30"
-    my_example_settings_json['vnics']=[]
+def main(writeSettingsFile, activeSettingsFile):
+    example_settings_json = {}
+    example_settings_json['topic_id']="null"
+    example_settings_json['multiprocess']="off"
+    example_settings_json['timeout_seconds']="30"
+    example_settings_json['vnics']=[]
     my_instance_metadata = get_current_metadata("instance")
     my_instance_vnic_metadata = get_current_metadata("vnics")
     my_bigip_interfaces = get_bigip_interfaces()
     print("Hostname: " + my_instance_metadata["hostname"])
     print("Instance ID: " + my_instance_metadata["id"])
     print("VNIC Count: " + str(len(my_instance_vnic_metadata)))
-    network_client = oci.core.VirtualNetworkClient(config={}, signer=signer)
+    try:
+        network_client = oci.core.VirtualNetworkClient(config={}, signer=signer)
+    except Exception as e:
+        print('Script failed to talk to OCI API, check routing to ensure request leaves primary instance VNIC & Security Groups are in-place: ' + str(e))
+        sys.exit(1)
     for vnic in my_instance_vnic_metadata:
         vnic_id = vnic['vnicId']
         primaryIP = vnic['privateIp']
-        vnicDetails = network_client.get_vnic(vnic_id).data
-        subnet_id = vnicDetails.subnet_id
-        subnetDetails = network_client.get_subnet(subnet_id).data
-        subnet_name = subnetDetails.display_name
-        private_ips = network_client.list_private_ips(vnic_id=vnic_id).data
+        try:
+            vnicDetails = network_client.get_vnic(vnic_id).data
+        except Exception as e:
+            print('Script failed to talk to OCI API, check routing to ensure request leaves primary instance VNIC & Security Groups are in-place: ' + str(e))
+            sys.exit(1)
+        vnic_name = vnicDetails.display_name
+        try:
+            private_ips = network_client.list_private_ips(vnic_id=vnic_id).data
+        except Exception as e:
+            print('Script failed to talk to OCI API, check routing to ensure request leaves primary instance VNIC & Security Groups are in-place: ' + str(e))
+            sys.exit(1)
         print("\n    VNIC: " + vnic['vnicId'])
         print("        MAC Addr: " + vnic['macAddr'])
-        print("        Subnet Name: " + subnet_name)
-        my_example_config_vnics = {}
-        my_example_config_vnics['ip_to_move'] = []
-        my_example_config_vnics['move_to_vnic'] = vnic['vnicId']
-        add_example_config = FALSE
+        print("        VNIC Name: " + vnic_name)
+        example_config_vnics = {}
+        example_config_vnics['ip_to_move'] = []
+        example_config_vnics['move_to_vnic'] = vnic['vnicId']
+        add_example_config = False
         for interface in my_bigip_interfaces:
             if interface['macAddress'].lower() == vnic['macAddr'].lower():
                 print("        BIG-IP Interface: " + interface['name'])
                 if interface['name'] != "mgmt":
-                    my_example_config_vnics['name'] = subnet_name
+                    example_config_vnics['vnic_name'] = vnic_name
+                    example_config_vnics['bigip_name'] = interface['name']
         for privateIP in private_ips:
             if privateIP.is_primary:
                 print("        Primary Private IP: " + privateIP.ip_address )
@@ -94,13 +108,19 @@ def main():
             else:
                 print("        Additional Secondary Private IP: " + privateIP.ip_address )
                 print("            ocid: " + privateIP.id )
-                if my_example_config_vnics['name']:
-                    add_example_config = TRUE
-                    my_example_config_vnics['ip_to_move'].append(privateIP.id)
-        if add_example_config == TRUE:
-            my_example_settings_json['vnics'].append(my_example_config_vnics)
+                add_example_config = True
+                example_config_vnics['ip_to_move'].append(privateIP.id)
+        if add_example_config == True:
+            example_settings_json['vnics'].append(example_config_vnics)
     print("\nExample settings.json config:")
-    print(json.dumps(my_example_settings_json,indent=4,sort_keys=True))
+    print(json.dumps(example_settings_json,indent=4,sort_keys=True))
+    if writeSettingsFile:
+        settings_file_name = os.path.dirname(os.path.realpath(sys.argv[0])) + '/settings.json'
+        os.rename(settings_file_name, settings_file_name+'.bak')
+        settings_file = open(settings_file_name, 'w')
+        settings_file.write(json.dumps(example_settings_json,indent=4,sort_keys=True))
+        settings_file.close()
+        print("Wrote settings to file: " + settings_file_name + " backup created with .bak extension.")
     sys.exit(0)
 
 # Get Instance Principal Token
@@ -112,4 +132,25 @@ except Exception as e:
 
 # Call Main
 if __name__ == '__main__':
-    main()
+    writeSettingsFile = False
+    activeSettingsFile = ""
+    try:
+        opts, args = getopt.getopt(sys.argv[1:],"hwa:",["writeSettings","activeSettingsFile"])
+    except getopt.GetoptError:
+        print('f5-vip-discovery.py')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('f5-vip-discovery.py: A tool to discover OCIDs for BIG-IP VNICs and Secondary IPs to aid in configuration of the BIG-IP failover script.')
+            print('To run discovery but not write settings file:')
+            print('\t f5-vip-discovery.py')
+            print('To run discovery but AND write discovered config to settings file:')
+            print('\t f5-vip-discovery.py -w')
+            print('To run discovery using secondary IPs from the Active device\'s settings file:')
+            print('\t f5-vip-discovery.py -a <active-bigip-settings.json>')
+            sys.exit()
+        elif opt in ("-w", "--writeSettings"):
+            writeSettingsFile = True
+        elif opt in ("-a", "--activeSettingsFile"):
+            activeSettingsFile = arg
+    main(writeSettingsFile, activeSettingsFile)
